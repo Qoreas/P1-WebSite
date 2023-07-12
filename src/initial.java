bdcc7649d24beb63966d05d5edc2e80353e643f8
Qoreas <buyfrtscbhbivpz@gmail.com>
2023-07-12 20:39:20 +0300

    initial

--- src/initial.java
+++ src/initial.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class initial {
+import javax.swing.*;
+
+public class initial {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
 }
